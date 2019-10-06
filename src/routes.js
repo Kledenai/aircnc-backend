68a0a7cadb78863575415089c32127ce38cc197c
kledenai <bruno.silva1643@etec.sp.gov.br>
2019-10-05 22:09:21 -0300

    rota com spot

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const AuthController = require("./controllers/AuthController");
+const SpotController = require("./controllers/SpotController");
 
 const routes = express.Router();
 
 routes.post("/users", AuthController.store);
+
+routes.post("/spots", SpotController.store);
 
 module.exports = routes;
